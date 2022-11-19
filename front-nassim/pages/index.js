9f52ffb67a5486a9cc34806978ae985c8e7d679f
Thiago <thiago.fm2@hotmail.com>
2022-11-19 15:47:07 -0300

    first commit with next js in frontend

---
+++ front-nassim/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+    <ul>
+      <li><a href="">Nassim</a></li>
+      <li><a href="">inicio</a></li>
+      <li><a href="">sobre</a></li>
+      <li><a href="">serviços</a></li>
+      <li><a href="">contato</a></li>
+    </ul>
+    <h1>nextjs</h1>
+    </>
+  )
+}
